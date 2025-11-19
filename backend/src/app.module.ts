baf5affaf38423c25fb1521b620185743f007bc8
NguyenBk22 <150934815+NguyenBk22@users.noreply.github.com>
2025-11-19 10:01:39 +0700

    init

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [AuthModule, AccountModule],
+
+})
+export class AppModule {}
