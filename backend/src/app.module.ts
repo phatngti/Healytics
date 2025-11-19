644ff7c22d30b8a14174b9cdeaa90d9216913ccd
NguyenBk22 <150934815+NguyenBk22@users.noreply.github.com>
2025-11-19 10:40:34 +0700

    config postgre database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
-
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [AuthModule, AccountModule],
-
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db = configService.get('database') as Record<string, any>;
+        return {
+          type: 'postgres',
+          host: db.host,
+          port: db.port,
+          username: db.username,
+          password: db.password,
+          database: db.database,
+          entities: db.entities,
+          synchronize: db.synchronize,
+        } as const;
+      },
+    }),
+    AuthModule,
+    AccountModule,
+  ],
 })
 export class AppModule {}
