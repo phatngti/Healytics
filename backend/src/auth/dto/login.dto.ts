5efd449c304f72f452f5d4a4dabd498d630b57a2
coder <coder@nonames-Mac-mini.local>
2025-11-22 00:03:09 +0700

    add: swagger config

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,9 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'test@example.com',
+    description: 'The email of the user',
+    format: 'email',
+    type: String,
+    required: true,
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'password',
+    description: 'The password of the user',
+    format: 'password',
+    type: String,
+    required: true,
+  })
   @IsNotEmpty()
   password: string;
 }
