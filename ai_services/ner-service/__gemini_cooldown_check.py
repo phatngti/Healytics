07a739efcf634479ea10e2f9aaab90e098da3344
NguyenBk22 <150934815+NguyenBk22@users.noreply.github.com>
2026-03-19 14:23:29 +0700

    v10

--- ai_services/ner-service/__gemini_cooldown_check.py
+++
@@ -1,5 +0,0 @@
-﻿from app.ner.gemini_ner import extract_entities_with_gemini
-
-q = "massage gần tôi ở quận 3 hcm"
-print("CALL 1:", extract_entities_with_gemini(q))
-print("CALL 2:", extract_entities_with_gemini(q))
